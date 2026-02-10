29754ab285d0d13be587e547c261cc40f20b07bd
varunvallabhaneni234-source <varunvallabhaneni234@gmail.com>
2026-02-10 12:31:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,16 @@ import pandas as pd
 import requests
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+st.text(smoothiefroot_response)
+
+sf_json = smoothiefroot_response.json()
+st.json(sf_json)
+
+sf_df = pd.DataFrame([sf_json])
+st.dataframe(sf_df)
